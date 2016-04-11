b82d4b0bcbdb56cefa394356e45a2d4196613bc7
SamDurfey <samuel.durfey@gmail.com>
2016-04-11 15:07:21 -0700

    Added html, js, and css files to begin work on French Castle.

---
+++ js/french.js
@@ -0,0 +1,11 @@
+// Business Logic
+// How the hell am I going to go about this?
+// You happen upon a castle. You decide it would behoove you to make an ally and strengthen your numbers.
+
+
+// UI Logic
+$(document).ready(function() {
+
+// stuff goes here.
+
+});

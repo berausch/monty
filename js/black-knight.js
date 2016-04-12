07366e74fc974f069ad13fa7f9ee42e98b03f731
SamDurfey <samuel.durfey@gmail.com>
2016-04-12 15:10:00 -0700

    Begin work on Black Knight gag. Super-basic hypothetical blackKnight and player objects.

---
+++ js/black-knight.js
@@ -0,0 +1,32 @@
+//Bidness
+//Setting up opponent...
+var blackKnight = {
+	hp: 100,
+	leftArm: true,
+	rightArm: true,
+	leftArm: true,
+	rightLeg: true,
+	insult: function() {
+		console.log("You fight like a dairy farmer.");
+	}
+};
+
+//Setting up player...? //do we want to ?
+var player = {
+	hp: 100,
+	swing: function() {
+		console.log("saaaawing battahhhh");
+	},
+	parry: function() {
+		console.log("parry. thrrrust. parry.");
+	},
+	insult: function() {
+		console.log("How appropriate, you fight like a cow.");
+	}
+};
+
+//UI Logic
+$(document).ready(function() {
+	// killing time >:3
+
+});

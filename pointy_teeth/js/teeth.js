aae139a4b8954ad324ca96ecd8a94b0c0985a5e9
c.g. Acharjya <captain@stratagemstudios.com>
2016-04-15 08:23:26 -0700

    cleanup etc

--- pointy_teeth/js/teeth.js
+++ pointy_teeth/js/teeth.js
@@ -1,14 +1,4 @@
 
-    google: { families: [ 'Rye::latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })(); 
  
 var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {preload: preload, create: create, update: update});
 
